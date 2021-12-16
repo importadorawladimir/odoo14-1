47c4e492cfea1166fcd17677a444f54790ed5236
importadorawladimir <85511327+importadorawladimir@users.noreply.github.com>
2021-12-15 22:48:25 -0500

    Merge Modulo de facturacion electronica

---
+++ l10n_ec_sri/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "SRI - Emision de comprabantes electronicos",
+    'summary': """SRI Emisión de comprobantes electrónicos off-line.""",
+    'author': 'Ekuasoft Group Solutions',
+    'maintainer': 'Ekuasoft Group Solutions',
+    'website': 'http://www.ekuasoft.com',
+    'license': 'AGPL-3',
+    'category': 'Account',
+    'depends': [
+        'base',
+        'account_edi',
+        'l10n_latam_invoice_document',
+        #'stock_picking_invoice_link',
+        #'partner_contact_tradename',
+        #'account_invoice_discount_amount',
+        #'base_mail_custom_attachment',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'data/ambiente.xml',
+        #'views/account_invoice.xml',
+        'views/res_company.xml',
+        #'views/sri.xml',
+        #'views/report_paper_format.xml',
+        #'views/account_invoice_report.xml',
+        #'data/invoice_action_data.xml',
+        'data/data.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'installable': True,
+}
